f0dad102689a0ab135a462b4077c0949e32035a6
Dennis Chung <chungdennis6@gmail.com>
2019-02-18 01:50:10 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  42 / 7
 end
 
 def assign_variable(value)
-  
+ name = "Bob"
 end
 
 def argue
-  
+return "#{phrase}"
 end
 
 def greeting
-
+ puts greeting = "Hi there,"
+  return name = "Bobby!"
 end
 
 def return_a_value
-
+  return phrase = "Nice"
 end
 
 def last_evaluated_value
-
+ return phrase = "expert"
 end
 
 def pizza_party
-
+  return "#{topping}"
 end
